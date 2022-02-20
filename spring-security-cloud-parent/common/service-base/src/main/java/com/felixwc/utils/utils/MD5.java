231b39a372b3ca9a9853342219c7b4010a8f40d5
cleboy <1192136710@qq.com>
2022-02-20 21:01:42 +0800

    1

---
+++ spring-security-cloud-parent/common/service-base/src/main/java/com/felixwc/utils/utils/MD5.java
@@ -0,0 +1,13 @@
+package com.felixwc.utils.utils;
+
+/**
+ * in order to learn java!
+ * created at 2022/1/1 21:36
+ *
+ * @author wangchao
+ */
+public class MD5 {
+    public static void test(){
+
+    }
+}
---
+++ spring-security-cloud-parent/infrastructure/api-gateway/src/main/java/com/felixwc/cloud/gateway/BootGatewayApp.java
@@ -0,0 +1,11 @@
+package com.felixwc.cloud.gateway;
+
+/**
+ * in order to learn java!
+ * created at 2022/1/4 14:14
+ *
+ * @author wangchao
+ */
+
+public class BootGatewayApp {
+}
